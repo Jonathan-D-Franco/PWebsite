2e1fa3893264505c17206a6ee7a9eba27c7b1dd3
BeepingJohnson <jdfranco@asu.edu>
2023-05-23 12:13:43 -0700

    formatting fixes

--- aboutMe.js
+++ aboutMe.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     let nestedRing = document.getElementById('nestRing');
+    let description = document.getElementById('info');
     nestedRing.addEventListener('click', function(event) {
         event.stopPropagation(); // stop event from bubbling up to parent elements
         this.classList.toggle('enlarge');
+        if (this.classList.contains('enlarge')) {
+        description.innerText = "Hello! I\'m a Computer Science student with a passion for coding. On this website, you\'ll find information about my skills, projects, and experiences.";
+        description.style.textAlign = "center";
+        }
+        else
+        {
+            description.innerText = "About Me";   
+        }
         console.log('clicked');
     });
 });
