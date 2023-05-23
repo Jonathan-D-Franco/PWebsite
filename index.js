7db38fe4e4e1592d98d24f56dccc7bcc4d21c9ff
BeepingJohnson <jdfranco@asu.edu>
2023-05-22 20:42:54 -0700

    Skill ring complete (for now)

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-let skills = document.getElementsByClassName('skill');
+document.addEventListener("DOMContentLoaded", function() {
+    let skills = Array.from(document.getElementsByClassName('skill'));
+    let skillRing = document.querySelector('.skill-ring');
 
-for (let i = 0; i < skills.length; i++) {
-  skills[i].style.transform = 'rotate(' + (i * 360 / skills.length) + 'deg)';
-  skills[i].style.display = 'block';
-}
+    // Calculate the center of the skill ring
+    const centerX = skillRing.getBoundingClientRect().left + skillRing.offsetWidth / 2;
+    const centerY = skillRing.getBoundingClientRect().top + skillRing.offsetHeight / 2;
 
-document.querySelector('.skill-ring').addEventListener('mouseover', function(event) {
-  if (event.target.classList.contains('skill')) {
-    document.getElementById('skill-display').innerText = 'You are now hovering over ' + event.target.dataset.skill;
-  }
-});
+    skillRing.addEventListener('mousemove', function(event) {
+        // Calculate the angle between the cursor and the horizontal axis
+        const angle = Math.atan2(event.clientY - centerY, event.clientX - centerX);
 
-document.querySelector('.skill-ring').addEventListener('mouseout', function(event) {
-  if (event.target.classList.contains('skill')) {
-    document.getElementById('skill-display').innerText = '';
-  }
+        // Convert the angle into a value between 0 and 1
+        const normalizedAngle = angle / (2 * Math.PI) + 0.5;
+
+        // Determine which skill to show
+        const skillIndex = Math.floor(normalizedAngle * skills.length);
+
+        // Hide all skills and show the selected one
+        skills.forEach((skill, index) => {
+            if (index === skillIndex) {
+                skill.style.opacity = '1';
+            } else {
+                skill.style.opacity = '0';
+            }
+        });
+    });
+
+    skillRing.addEventListener('mouseout', function() {
+        // Hide all skills when the cursor leaves the ring
+        skills.forEach(skill => skill.style.opacity = '0');
+    });
 });
