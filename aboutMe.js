8afc84f73f0ca21e9d39c005e866becbc757a604
BeepingJohnson <jdfranco@asu.edu>
2023-06-07 16:41:18 -0700

    editing desciption

--- aboutMe.js
+++ aboutMe.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         event.stopPropagation(); // stop event from bubbling up to parent elements
         this.classList.toggle('enlarge');
         if (this.classList.contains('enlarge')) {
-        description.innerText = "Hello! I\'m a Computer Science student with a passion for coding. On this website, you\'ll find information about my skills, projects, and experiences.";
+        description.innerText = "Hello, hopefully potential future employer! I\'m a newly graduated computer science with a passion for coding and a drive to learn. On this website, you\'ll find information about my skills, projects, and experiences. This is an ongoing project and hopefully will show what I've learned as time goes by";
         description.style.textAlign = "center";
         }
         else
